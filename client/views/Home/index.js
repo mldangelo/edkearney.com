a0ed3cce7f38717317fc12263f3b750a08826d23
mldangelo <michael.l.dangelo@gmail.com>
2018-02-23 03:09:54 -0500

    initial commit of boilerplate code

---
+++ client/views/Home/index.js
@@ -0,0 +1,14 @@
+import Counter from 'components/Counter';
+
+export default {
+  render(h) {
+    return (
+      <div className="page">
+        <Counter />
+        <p>
+          To get started, edit files in <code>./client</code> and save.
+        </p>
+      </div>
+    );
+  },
+};
